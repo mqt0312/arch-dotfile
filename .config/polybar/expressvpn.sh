5a2f705885c95123e718d8779c38b3958d014880
Minh Truong <mqt@bu.edu>
2019-09-04 00:52:31 -0400

    First batch, hopefully last!

---
+++ .config/polybar/expressvpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IS_CONNECTED=""
+NOT_CONNECTED="￦"
+VPN_STATUS=$(expressvpn status | grep -c "Connected to");
+REAL_IP="$(timeout 3 curl -s ifconfig.me)"
+if [ $((VPN_STATUS)) = 1 ]; then	
+	echo "$IS_CONNECTED $REAL_IP" 
+else
+	echo "$NOT_CONNECTED $REAL_IP"
+fi
