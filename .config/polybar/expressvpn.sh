2df714be66b2da87ce1589d1090a482653ba0f3c
Minh Truong <mqt@bu.edu>
2020-01-24 01:08:34 -0500

    added new files

--- .config/polybar/expressvpn.sh
+++ .config/polybar/expressvpn.sh
@@ -2,7 +2,7 @@
 
 IS_CONNECTED=""
 NOT_CONNECTED="￦"
-VPN_STATUS=$(expressvpn status | grep -c "Connected to");
+VPN_STATUS="$(expressvpn status 2>&1 | grep -c "Connected to")";
 REAL_IP="$(timeout 3 curl -s ifconfig.me)"
 if [ $((VPN_STATUS)) = 1 ]; then	
 	echo "$IS_CONNECTED $REAL_IP" 
